93dbbfb1751e8ffd19edb73519ec49ce199b7bc9
wighawag <wighawag@gmail.com>
2022-03-21 18:26:25 +0000

    add patch for RCP node issues

--- web/.setup.cjs
+++ web/.setup.cjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const fs = require('fs');
+const fs = require('fs-extra');
 function copyFromDefault(p) {
   if (!fs.existsSync(p)) {
     const defaultFile = `${p}.default`;
@@ -10,3 +10,30 @@ function copyFromDefault(p) {
 }
 
 [/*'.vscode/settings.json', */ '.env'].map(copyFromDefault);
+
+// from https://stackoverflow.com/questions/26827920/nodejs-recursively-list-files-in-directory/26828357#26828357
+function walk(directoryName, fnc) {
+  fs.readdir(directoryName, function (e, files) {
+    if (e) {
+      return;
+    }
+    files.forEach(function (file) {
+      var fullPath = directoryName + '/' + file;
+      fs.stat(fullPath, function (e, f) {
+        if (e) {
+          return;
+        }
+        if (f.isDirectory()) {
+          walk(fullPath, fnc);
+        } else {
+          fnc(fullPath);
+        }
+      });
+    });
+  });
+}
+
+const overridesFolder = 'overrides';
+walk(overridesFolder, (fullpath) =>
+  fs.copyFileSync(fullpath, `node_modules/${fullpath.substring(overridesFolder.length + 1)}`)
+);
