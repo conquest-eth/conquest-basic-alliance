0bbf94ea15219df1487597a7cfde3776fc8684c7
wighawag <wighawag@gmail.com>
2024-12-18 12:20:21 +0000

    replace agent-service with missiv

--- web/src/lib/messages/message.ts
+++ web/src/lib/messages/message.ts
@@ -5,12 +5,12 @@ export type MessageFlow = {
   step: 'IDLE' | 'LOADING' | 'READY';
   owner?: string;
   profile?: {
-    description?: string;
+    domainDescription?: string;
   };
   error?: {message?: string};
 };
 
-const PROFILE_URI = import.meta.env.VITE_PROFILE_URI as string;
+const MISSIV_URI = import.meta.env.VITE_MISSIV_URI as string;
 
 class MessageFlowStore extends BaseStoreWithData<MessageFlow, undefined> {
   public constructor() {
@@ -24,22 +24,30 @@ class MessageFlowStore extends BaseStoreWithData<MessageFlow, undefined> {
     this.setPartial({step: 'LOADING', owner});
     try {
       // TODO CACHE data
-      const response = await fetch(`${PROFILE_URI}get/${owner}`, {
-        method: 'GET',
+      const response = await fetch(`${MISSIV_URI}/api/user/getCompleteUser`, {
+        method: 'POST',
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
+        body: JSON.stringify({
+          domain: 'conquest.eth',
+          address: owner.toLowerCase(),
+        }),
       });
       const json = await response.json();
-      const result: {
-        account: {
-          description?: string;
-          publicEncryptionKey: string;
-          publicSigningKey: string;
-          nonceMsTimestamp: number;
-        } | null;
-      } = json;
-      this.setPartial({step: 'READY', owner, profile: result.account});
+
+      if (!json.completeUser) {
+        // fallback case to let people connect to player we used the old account-service and where still present on the switch
+        if (owner.toLowerCase() === '0x4b9d53246ed18db31f26fc59b6e47a9efc3c1213') {
+          this.setPartial({step: 'READY', owner, profile: {domainDescription: 'LukaskywaIker'}});
+          return;
+        } else if (owner.toLowerCase() === '0x88c0558cb8525c88f78752bb0bdc3e6221597165') {
+          this.setPartial({step: 'READY', owner, profile: {domainDescription: 'anar'}});
+          return;
+        }
+      }
+
+      this.setPartial({step: 'READY', owner, profile: json.completeUser});
     } catch (e) {
       this.setPartial({error: e});
     }
